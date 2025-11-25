3cf8943b7c2f78aa4026b51184c46c45d4454bef
saraswatkartik <kartiksaraswat882003@gmail.com>
2025-11-25 11:41:57 +0530

    added some  js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 // <!-- js start -->
-
+// for dropdown menu
 const toggleBtn = document.querySelector('.toggle-btn');
 const dropdownMenu = document.querySelector('.dropdown-menu');
 
 toggleBtn.addEventListener('click', () => {
     dropdownMenu.classList.toggle('top-16');
 });
+
+
+// for name animation
+var typed = new Typed('#type-name', {
+    strings: ['Kartik Saraswat', 'Web Developer', 'Web Designer'],
+    typeSpeed: 100,
+    loop: true,
+});
+
 
 // <!-- js end -->
